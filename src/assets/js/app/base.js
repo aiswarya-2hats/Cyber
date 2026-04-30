c1b336178f90dea93eadf195ee6cc0238dc4e6c1
Aiswarya <aiswarya@2hatslogic.com>
2026-04-30 14:44:32 +0530

    fix: sticky header on scroll back to top

--- src/assets/js/app/base.js
+++ src/assets/js/app/base.js
@@ -1,1 +1,38 @@
-console.log('base loaded!');
+const siteHeader = document.querySelector(".site-header");
+
+if (siteHeader) {
+  let lastScrollY = window.scrollY;
+  const topThreshold = 10;
+  let isTicking = false;
+
+  const updateHeaderState = () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY <= topThreshold) {
+      siteHeader.classList.add("is-sticky");
+      siteHeader.classList.remove("is-hidden");
+    } else {
+      if (currentScrollY < lastScrollY) {
+        siteHeader.classList.add("is-sticky");
+        siteHeader.classList.remove("is-hidden");
+      } else if (currentScrollY > lastScrollY) {
+        siteHeader.classList.add("is-hidden");
+      }
+    }
+
+    lastScrollY = currentScrollY;
+    isTicking = false;
+  };
+
+  updateHeaderState();
+  window.addEventListener(
+    "scroll",
+    () => {
+      if (!isTicking) {
+        window.requestAnimationFrame(updateHeaderState);
+        isTicking = true;
+      }
+    },
+    { passive: true }
+  );
+}
